c9420ccd4ba47336478318593fff4146bb2e3780
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-11-17 17:19:35 +0100

    Initial commit: display some products

---
+++ android/app/src/main/kotlin/com/jeremiahai/shop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jeremiahai.shop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  ProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        title: Text(
+          product.title,
+          textAlign: TextAlign.center,
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+        // subtitle: Text(product.description),
+      ),
+    );
+  }
+}
