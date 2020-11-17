bee9364d91749f20bdd83b0273bf651de591da65
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-11-17 23:33:21 +0100

    Display order details

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -1,10 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop_app/providers/orders.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final Order order;
+
   const OrderItemWidget({Key key, this.order}) : super(key: key);
+
+  @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  var expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +23,55 @@ class OrderItemWidget extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount.toStringAsFixed(2)}'),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime)),
+            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+            subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: AnimatedSwitcher(
+                  duration: Duration(milliseconds: 200),
+                  // transitionBuilder: (child, animation) {
+                  //   return RotationTransition(
+                  //     turns: animation,
+                  //     child: child,
+                  //   );
+                  // },
+                  child: Icon(
+                    expanded ? Icons.expand_less : Icons.expand_more,
+                    key: ValueKey(expanded),
+                  )),
+              onPressed: () {
+                setState(() {
+                  expanded = !expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (expanded)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: min(widget.order.products.length * 20.0 + 10, 100),
+                child: ListView(
+                  children: widget.order.products
+                      .map((prod) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                prod.title,
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '${prod.quantity} x \$${prod.price.toStringAsFixed(2)}',
+                                style:
+                                    TextStyle(fontSize: 18, color: Colors.grey),
+                              )
+                            ],
+                          ))
+                      .toList(),
+                ),
+              ),
+            )
         ],
       ),
     );
