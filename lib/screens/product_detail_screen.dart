ef01f2d1fe5a4d421d27198ffbfe5ef6d56ca255
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-11-17 22:21:16 +0100

    Miniature implementation of details page

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -3,12 +3,49 @@ import 'package:shop_app/providers/product.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = '/product-details';
+
   @override
   Widget build(BuildContext context) {
     final product = ModalRoute.of(context).settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "\$${product.price.toStringAsFixed(2)}",
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "${product.description}",
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                  style: TextStyle(color: Colors.grey, fontSize: 20),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
