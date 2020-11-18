f1d9b195485a0cb329a08d1372fec567a54db901
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-11-18 13:51:03 +0100

    Use FutureBuilder to manage loading state on orders screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,26 +4,8 @@ import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item_widget.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static String routeName = '/orders';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    _isLoading = true;
-    //  We can use of(context) here because we're not listening. If we were listening, we'd have had to use a Future.delayed.. or worked with didChangeDependencies and tracking if the page is loaded
-    Provider.of<Orders>(context, listen: false)
-        .fetchAndSetOrders()
-        .then((value) => setState(() => _isLoading = false));
-
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,17 +14,25 @@ class _OrdersScreenState extends State<OrdersScreen> {
       appBar: AppBar(
         title: Text('Your orders'),
       ),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Consumer<Orders>(
-              builder: (_, ordersData, child) => ListView.builder(
-                itemCount: ordersData.orders.length,
-                itemBuilder: (BuildContext context, int index) =>
-                    OrderItemWidget(order: ordersData.orders[index]),
-              ),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (_, dataSnapshot) =>
+            dataSnapshot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : dataSnapshot.hasError
+                    ? Center(
+                        child: Text('An error occurred'),
+                      )
+                    : Consumer<Orders>(
+                        builder: (_, ordersData, child) => ListView.builder(
+                          itemCount: ordersData.orders.length,
+                          itemBuilder: (BuildContext context, int index) =>
+                              OrderItemWidget(order: ordersData.orders[index]),
+                        ),
+                      ),
+      ),
     );
   }
 }
