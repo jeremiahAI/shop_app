644651be4d8aec72e66cc41eac8fef688a131ab0
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-11-22 09:08:12 +0100

    ADd fade in animation for Images

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -24,8 +24,9 @@ class ProductItem extends StatelessWidget {
           child: GestureDetector(
             onTap: () => Navigator.of(context)
                 .pushNamed(ProductDetailsScreen.routeName, arguments: product),
-            child: Image.network(
-              product.imageUrl,
+            child: FadeInImage(
+              placeholder: AssetImage('images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
               fit: BoxFit.cover,
             ),
           ),
