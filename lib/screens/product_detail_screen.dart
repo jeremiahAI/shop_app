04a8db528ed95e6dff9e79267f301263e37616cb
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-11-22 09:18:25 +0100

    Add collapsible toolbar animation on detail page

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -8,16 +8,14 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final product = ModalRoute.of(context).settings.arguments as Product;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.title),
+              background: Hero(
                 tag: product.id,
                 child: Image.network(
                   product.imageUrl,
@@ -25,30 +23,35 @@ class ProductDetailsScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "\$${product.price.toStringAsFixed(2)}",
-              style: TextStyle(color: Colors.grey, fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "${product.description}",
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                  style: TextStyle(color: Colors.grey, fontSize: 20),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "\$${product.price.toStringAsFixed(2)}",
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "${product.description}",
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    style: TextStyle(color: Colors.grey, fontSize: 20),
+                  ),
                 ),
               ),
-            )
-          ],
-        ),
+            ]),
+          ),
+        ],
       ),
     );
   }
